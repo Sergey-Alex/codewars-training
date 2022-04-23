fe2b9e783925333436162eec41269a0a055f4519
Sergey-Alex <strotelevsergwyalex@gmail.com>
2022-04-23 20:25:07 +0300

    opposite

--- index.js
+++ index.js
@@ -9,19 +9,31 @@
 // }
 //
 // console.log(isSquare(-1))
+//
+// function digPow(n, p){
+//     let strN = String(n).split('');
+//     let summ= 0
+//     for (let i = 0; i < strN.length; i++){
+//           summ+=Math.pow(Number(strN[i]), p)
+//         p++
+//     }
+//     let data = summ / n
+//     if(summ%n === 0){
+//         return data
+//     }else {
+//         return  -1
+//     }
+// }
+// console.log(digPow(89, 1))
 
-function digPow(n, p){
-    let strN = String(n).split('');
-    let summ= 0
-    for (let i = 0; i < strN.length; i++){
-          summ+=Math.pow(Number(strN[i]), p)
-        p++
-    }
-    let data = summ / n
-    if(summ%n === 0){
-        return data
-    }else {
-        return  -1
-    }
-}
-console.log(digPow(89, 1))
+// function opposite(number) {
+//   if (number > 0){
+//       return  -number
+//   } else if (number < 0) {
+//       return  Math.abs(number)
+//   } else if (number == 0){
+//       return  null
+//   }
+// }
+//
+// console.log(opposite(0))
