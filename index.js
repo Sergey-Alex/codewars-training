046d3992c09f1c0c643f53d78d06c36cfc63a1ee
Sergey-Alex <strotelevsergwyalex@gmail.com>
2022-04-23 20:10:40 +0300

    Playing with digits

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-let isSquare = function(n){
-    if (n%Math.sqrt(n) == 0 || Math.sqrt(n) === 0){
-        return true
-    } else {
-        return false
+// let isSquare = function(n){
+//     if (n%Math.sqrt(n) == 0 || Math.sqrt(n) === 0){
+//         return true
+//     } else {
+//         return false
+//     }
+//
+//
+// }
+//
+// console.log(isSquare(-1))
+
+function digPow(n, p){
+    let strN = String(n).split('');
+    let summ= 0
+    for (let i = 0; i < strN.length; i++){
+          summ+=Math.pow(Number(strN[i]), p)
+        p++
     }
-
-
+    let data = summ / n
+    if(summ%n === 0){
+        return data
+    }else {
+        return  -1
+    }
 }
-
-console.log(isSquare(-1))
+console.log(digPow(89, 1))
